776834d8a7474d45887dc64b0adb9a1650f17cdb
franklin <frank.fkn.23@gmail.com>
2025-02-18 15:24:34 -0300

    resolvendo exercicio exclamation_marks

---
+++ src/code_problems/code_wars/exclamation_marks_series.py
@@ -0,0 +1,12 @@
+vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+def change_to_exclamation_mark(text: str) -> str:
+    final_text = []
+    for char in text:
+        if char in vowels:
+            final_text.append('!')
+        else:
+            final_text.append(char)
+
+    return ''.join(final_text)
+
