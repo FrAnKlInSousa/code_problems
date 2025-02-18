2cbeec2d7c1435e7338afb6d0c660741f28ae2a8
franklin <frank.fkn.23@gmail.com>
2025-02-18 15:25:00 -0300

    rodando lint no diretório de testes

--- tests/test_code_wars/test_array_diff.py
+++ tests/test_code_wars/test_array_diff.py
@@ -1,16 +1,22 @@
 import pytest
 
-from src.code_problems.code_wars.array_diff import array_diff, array_diff_clever
+from src.code_problems.code_wars.array_diff import (
+    array_diff,
+    array_diff_clever,
+)
 
 
-@pytest.mark.parametrize('array_a,array_b,expected', [
-    ([1,2],[1], [2]),
-    ([1,2,2], [1], [2, 2]),
-    ([1,2,2], [2], [1]),
-    ([1,2,2], [], [1,2,2]),
-    ([], [1, 2], []),
-    ([1, 2, 3], [1, 2], [3])
-])
+@pytest.mark.parametrize(
+    'array_a,array_b,expected',
+    [
+        ([1, 2], [1], [2]),
+        ([1, 2, 2], [1], [2, 2]),
+        ([1, 2, 2], [2], [1]),
+        ([1, 2, 2], [], [1, 2, 2]),
+        ([], [1, 2], []),
+        ([1, 2, 3], [1, 2], [3]),
+    ],
+)
 @pytest.mark.parametrize('function', [array_diff, array_diff_clever])
 def test_array_diff(array_a, array_b, expected, function):
     result = function(array_a, array_b)
--- tests/test_code_wars/test_naughty_number.py
+++ tests/test_code_wars/test_naughty_number.py
@@ -1,22 +1,39 @@
 import pytest
 
-from src.code_problems.code_wars.naughty_number import naughty_number, naughty_number_clever, naughty_number_gpt
-
-
-@pytest.mark.parametrize('arr,expected', [
-    ([ [[[]]] , [[]], [], [], [[2]] ], 4),
-    ([ [1] ], 0),
-    ([ [], [8], [] , [] ], 1),
-    ([[[[[[[[[42]]]]]]]]], 0),
-    ([ [], [], [] , [], [], [], [], [], [], [], [] , [77]], 11),
-    ([ [[]], [[[[[]]]]], [] , [[[[[[[[[]]]]]]]]],[[[]]], [[[[31]]]],[[]],[] ], 5),
-    ([ [1], [[[[]]]],[[]],[[[[[[[[[]]]]]]]]],[] ], 0)
-])
-@pytest.mark.parametrize('function', [
+from src.code_problems.code_wars.naughty_number import (
     naughty_number,
     naughty_number_clever,
-    naughty_number_gpt
-])
+    naughty_number_gpt,
+)
+
+
+@pytest.mark.parametrize(
+    'arr,expected',
+    [
+        ([[[[]]], [[]], [], [], [[2]]], 4),
+        ([[1]], 0),
+        ([[], [8], [], []], 1),
+        ([[[[[[[[[42]]]]]]]]], 0),
+        ([[], [], [], [], [], [], [], [], [], [], [], [77]], 11),
+        (
+            [
+                [[]],
+                [[[[[]]]]],
+                [],
+                [[[[[[[[[]]]]]]]]],
+                [[[]]],
+                [[[[31]]]],
+                [[]],
+                [],
+            ],
+            5,
+        ),
+        ([[1], [[[[]]]], [[]], [[[[[[[[[]]]]]]]]], []], 0),
+    ],
+)
+@pytest.mark.parametrize(
+    'function', [naughty_number, naughty_number_clever, naughty_number_gpt]
+)
 def test_naughty_number(arr, expected, function):
     result = function(arr)
     assert result == expected
