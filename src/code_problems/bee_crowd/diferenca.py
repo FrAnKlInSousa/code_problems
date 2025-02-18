b0b9a062fc0d46b144f5a94daa458fcc95357be1
franklin <frank.fkn.23@gmail.com>
2025-02-17 21:10:34 -0300

    configurando poetry e adicionando algumas respostas

---
+++ src/code_problems/bee_crowd/diferenca.py
@@ -0,0 +1,12 @@
+# https://judge.beecrowd.com/pt/problems/view/1007
+
+def diff():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    diferenca = (a * b) - (c * d)
+    print(f'DIFERENCA = {diferenca}')
+
+diff()
---
+++ src/code_problems/bee_crowd/soma_simples.py
@@ -0,0 +1,11 @@
+# https://judge.beecrowd.com/pt/problems/view/1003
+
+def soma_dois_numeros():
+    num_1 = int(input())
+    num_2 = int(input())
+    soma = num_1 + num_2
+    return soma
+
+
+resultado = soma_dois_numeros()
+print(f'SOMA = {resultado}')
