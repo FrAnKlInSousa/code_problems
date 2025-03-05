443df14e5f244a6188279c90e4e73737186402f8
franklin <frank.fkn.23@gmail.com>
2025-03-05 13:27:36 -0300

    corrigindo print do nome do projeto

--- src/utils/create_problem_data.py
+++ src/utils/create_problem_data.py
@@ -1,6 +1,6 @@
 def format_name(name: str):
     name = name.lower().replace(' ', '_').replace('#', '')
-    test = 'test' + name
+    test = 'test_' + name
     print(name, test)
     return name
 
@@ -9,5 +9,5 @@ def table_data(name: str, level: int):
     print(
         f'| [{format_name(name)}.py]'
         f'(src/code_problems/code_wars/kyu_{level}/)'
-        f' | [{name}]() | {level} Kyu'
+        f' | [{name}]() | {level} Kyu |'
     )
