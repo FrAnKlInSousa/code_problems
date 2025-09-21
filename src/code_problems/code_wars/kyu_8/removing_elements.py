2c31412ea777a9f4b71443fc350e689653f1b744
franklin <frank.fkn.23@gmail.com>
2025-09-21 18:53:29 -0300

    exercicio removendo de 2 em 2 elementos

---
+++ src/code_problems/code_wars/kyu_8/removing_elements.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+
+def remove_every_other(my_list: list[Any]) -> list[Any]:
+    new_elements = []
+    for index, element in enumerate(my_list):
+        if index % 2 == 0:
+            new_elements.append(element)
+    return new_elements
--- tests/test_code_wars/test_removing_elements.py
+++ tests/test_code_wars/test_removing_elements.py
@@ -1,10 +1,15 @@
 import pytest
 
+from src.code_problems.code_wars.kyu_8.removing_elements import remove_every_other
 
-@pytest.mark.parametrize(',expected', [
-    (),
+
+@pytest.mark.parametrize('elements,expected', [
+    (["Keep", "Remove", "Keep", "Remove", "Keep"], ["Keep", "Keep", "Keep"]),
+    (['Hello', 'Goodbye', 'Hello Again'], ['Hello', 'Hello Again']),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 3, 5, 7, 9]),
+    ([[1, 2]], [[1, 2]])
 ])
-@pytest.mark.parametrize('function', [])
-def test_removing_elements(expected, function):
-    result = function()
+@pytest.mark.parametrize('function', [remove_every_other])
+def test_removing_elements(elements, expected, function):
+    result = function(elements)
     assert result == expected
