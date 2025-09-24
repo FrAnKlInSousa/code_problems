3a9526845b5646d4123792dd9c06834b7b1d8f99
franklin <frank.fkn.23@gmail.com>
2025-09-24 19:29:51 -0300

    adicionando outra resolucao do exercicio ensure question

--- src/code_problems/bee_crowd/diferenca.py
+++ src/code_problems/bee_crowd/diferenca.py
@@ -9,9 +9,13 @@ def diff():
 
     diferenca = (a * b) - (c * d)
     print(f'DIFERENCA = {diferenca}')
+
+
 # links para ajudar a testar prints:
-# https://www.youtube.com/watch?v=mjpJsKlF2-0 (live onde eu pergunto sobre testes com prints)
+# https://www.youtube.com/watch?v=mjpJsKlF2-0
+# (live onde eu pergunto sobre testes com prints)
 # https://exercicios.dunossauro.com/
 # https://www.youtube.com/watch?v=8uiMnwIkPYA
+
 
 diff()
--- src/code_problems/code_wars/kyu_8/ensure_question.py
+++ src/code_problems/code_wars/kyu_8/ensure_question.py
@@ -2,3 +2,7 @@ def ensure_question(phrase: str) -> str:
     if phrase:
         return f'{phrase}?' if phrase[-1] != '?' else phrase
     return f'{phrase}?'
+
+
+def ensure_question_clever(phrase: str) -> str:
+    return phrase if phrase.endswith('?') else phrase + '?'
