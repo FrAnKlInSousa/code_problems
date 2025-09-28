9921bdf645c37668fcb3229121fa6ec01d452f2c
franklin <frank.fkn.23@gmail.com>
2025-09-28 01:12:42 -0300

    fix PLR2004

--- src/code_problems/code_wars/kyu_8/remove_first_and_last_character_part_two.py
+++ src/code_problems/code_wars/kyu_8/remove_first_and_last_character_part_two.py
@@ -1,8 +1,11 @@
 def array(string: str) -> str | None:
     string = string.split(',')
+    min_len = 3
+
     def _remove_first_and_last(string_list: list) -> list:
-        return string_list[1:len(string)-1]
-    if len(string) < 3:
+        return string_list[1 : len(string) - 1]
+
+    if len(string) < min_len:
         return None
     string = _remove_first_and_last(string)
     return ' '.join(string)
