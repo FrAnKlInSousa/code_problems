8d78743b0f423055b5db2267ca9918ad11d9a556
franklin <frank.fkn.23@gmail.com>
2025-09-28 23:55:26 -0300

    aplicando lint

--- src/code_problems/code_wars/kyu_8/grader.py
+++ src/code_problems/code_wars/kyu_8/grader.py
@@ -1,21 +1,28 @@
+grade_a = 0.9
+grade_b = 0.8
+grade_c = 0.7
+grade_d = 0.6
+grade_f = 1
+
+
 def grader(score) -> str:
-    if score > 1 or score < 0.6:
+    if score > grade_f or score < grade_d:
         return 'F'
-    if score >= 0.9:
+    if score >= grade_a:
         return 'A'
-    if score >= 0.8:
+    if score >= grade_b:
         return 'B'
-    if score >= 0.7:
+    if score >= grade_c:
         return 'C'
-    if score >= 0.6:
+    if score >= grade_d:
         return 'D'
 
 
 def grader_clever(score) -> str:
     return 'FDCBAF'[
-        (score > 1)
-        + (score >= 0.9)
-        + (score >= 0.8)
-        + (score >= 0.7)
-        + (score >= 0.6)
+        (score > grade_f)
+        + (score >= grade_a)
+        + (score >= grade_b)
+        + (score >= grade_c)
+        + (score >= grade_d)
     ]
