367bad17866f74ce76f5e41ec9e4aa370a377b97
franklin <frank.fkn.23@gmail.com>
2025-10-01 14:39:56 -0300

    fix coverage

--- tests/test_code_wars/test_counting_sheep.py
+++ tests/test_code_wars/test_counting_sheep.py
@@ -5,11 +5,12 @@ from src.code_problems.code_wars.kyu_8.counting_sheep import count_sheeps
 
 @pytest.mark.parametrize('sheeps,expected', [
     ([True,  True,  True,  False,
-                  True,  True,  True,  True ,
-                  True,  False, True,  False,
-                  True,  False, False, True ,
-                  True,  True,  True,  True ,
-                  False, False, True,  True ], 17),
+      True,  True,  True,  True ,
+      True,  False, True,  False,
+      True,  False, False, True ,
+      True,  True,  True,  True ,
+      False, False, True,  True ], 17),
+    ([], 0)
 ])
 @pytest.mark.parametrize('function', [count_sheeps])
 def test_counting_sheep(sheeps, expected, function):
