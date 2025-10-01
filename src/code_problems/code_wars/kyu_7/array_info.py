694303a6985424963cffb93708c9ed43ffc97a82
franklin <frank.fkn.23@gmail.com>
2025-10-01 00:45:28 -0300

    exercicio array info

---
+++ src/code_problems/code_wars/kyu_7/array_info.py
@@ -0,0 +1,75 @@
+def _array_len(array):
+    return [len(array)]
+
+
+def _int_count(array):
+    count = len([item for item in array if type(item) is int])
+    if count == 0:
+        return [None]
+    return [count]
+
+
+def _float_count(array):
+    count = len([item for item in array if type(item) is float])
+    if count == 0:
+        return [None]
+    return [count]
+
+
+def _str_count(array):
+    def _remove_blank_spaces(string):
+        return ''.join(string.split())
+
+    count = len([
+        item
+        for item in array
+        if type(item) is str and _remove_blank_spaces(item)
+    ])
+    if count == 0:
+        return [None]
+    return [count]
+
+
+def _blank_spaces_count(array):
+    blank_spaces = 0
+
+    for item in array:
+        if type(item) is str:
+            blank_spaces += item.count(' ')
+    if blank_spaces == 0:
+        return [None]
+    return [blank_spaces]
+
+
+def array_info(array):
+    if not array:
+        return 'Nothing in the array!'
+
+    result = []
+    result.append(_array_len(array))
+    result.append(_int_count(array))
+    result.append(_float_count(array))
+    result.append(_str_count(array))
+    result.append(_blank_spaces_count(array))
+    return result
+
+
+def array_info_clever(array):
+    if not array:
+        return 'Nothing in the array!'
+    return [
+        [len(array)],
+        [sum(isinstance(item, int) for item in array) or None],
+        [sum(isinstance(item, float) for item in array) or None],
+        [
+            sum(isinstance(item, str) and not item.isspace() for item in array)
+            or None
+        ],
+        [
+            sum(isinstance(item, str) and item.isspace() for item in array)
+            or None
+        ],
+    ]
+
+
+print(array_info_clever([1, 2, 3.33, 4, 5.01, 'bass', 'kick', ' ']))
---
+++ tests/test_code_wars/test_array_info.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.code_problems.code_wars.kyu_7.array_info import array_info
+
+
+@pytest.mark.parametrize('array,expected', [
+    ([1, 2, 3.33, 4, 5.01, 'bass', 'kick', ' '], [[8], [3], [2], [2], [1]]),
+    ([0.001, 2, ' '], [[3], [1], [1], [None], [1]]),
+    ([], 'Nothing in the array!'),
+    ([' '], [[1], [None], [None], [None], [1]]),
+    ([0.001, 2, 'bass'], [[3], [1], [1], [1], [None]])
+])
+@pytest.mark.parametrize('function', [array_info])
+def test_array_info(array, expected, function):
+    result = function(array)
+    assert result == expected
