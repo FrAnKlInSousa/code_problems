c7c8b4b2c2411a4e9850f90a9f93f739a289666e
franklin <frank.fkn.23@gmail.com>
2025-10-02 17:20:37 -0300

    exercicio red knight

--- src/code_problems/code_wars/kyu_7/decade_80_s_kids_1_how_many_licks_does_it_take.py
+++ src/code_problems/code_wars/kyu_7/decade_80_s_kids_1_how_many_licks_does_it_take.py
@@ -7,8 +7,12 @@ def total_licks(env: dict):
     if high > 0:
         for k, v in env.items():
             if v == high:
-                return (f'It took {licks} licks to get '
-                        f'to the tootsie roll center of '
-                        f'a tootsie pop. The toughest challenge was {k}.')
-    return (f'It took {licks} licks to get to the '
-            f'tootsie roll center of a tootsie pop.')
+                return (
+                    f'It took {licks} licks to get '
+                    f'to the tootsie roll center of '
+                    f'a tootsie pop. The toughest challenge was {k}.'
+                )
+    return (
+        f'It took {licks} licks to get to the '
+        f'tootsie roll center of a tootsie pop.'
+    )
