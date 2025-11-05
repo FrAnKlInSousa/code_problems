44857d48279bc43bda76400213ac12a4b5a38da4
franklin <frank.fkn.23@gmail.com>
2025-11-05 17:53:10 -0300

    metodo get_next mais testes

--- src/code_problems/code_wars/kyu_6/harshad_or_niven_numbers.py
+++ src/code_problems/code_wars/kyu_6/harshad_or_niven_numbers.py
@@ -1,11 +1,19 @@
 class Harshad:
-    def is_valid(self, number: int):
+    @staticmethod
+    def is_valid(number: int):
         digits = list(str(number))
         digit_sum = sum(int(num) for num in digits)
+        if digit_sum == 0:
+            return True
         return number % digit_sum == 0
 
-    def get_next(self):
-        ...
+    @staticmethod
+    def get_next(number: int):
+        number += 1
+        while True:
+            if Harshad().is_valid(number):
+                return number
+            number += 1
 
-    def get_serie(self):
-        ...
+    @staticmethod
+    def get_serie(): ...
--- tests/test_code_wars/test_harshad_or_niven_numbers.py
+++ tests/test_code_wars/test_harshad_or_niven_numbers.py
@@ -5,7 +5,7 @@ from src.code_problems.code_wars.kyu_6.harshad_or_niven_numbers import Harshad
 harshad = Harshad()
 
 @pytest.mark.parametrize('number', [
-    10, 27, 588
+    0, 10, 27, 588
 ])
 def test_harshad_valid_numbers(number):
     assert harshad.is_valid(number)
@@ -16,3 +16,13 @@ def test_harshad_valid_numbers(number):
 ])
 def test_harshad_invalid_numbers(number):
     assert not harshad.is_valid(number)
+
+
+@pytest.mark.parametrize('number,expected', [
+    (0, 1),
+    (1, 2),
+    (17, 18),
+    (25, 27)
+])
+def test_next_harshad_numbers(number, expected):
+    assert harshad.get_next(number) == expected
